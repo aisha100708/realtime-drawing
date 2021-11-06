91493f0b35a24cd2eb9f39ae47b70a9e5d517ba3
aisha100708 <71584539+aisha100708@users.noreply.github.com>
2021-11-06 16:35:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 function setup() {
     canvas = createCanvas(600, 500);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(600, 500);
+    video.position(20, 200);
+    canvas.position(700, 200);
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', getPoses);
+}
+function draw() {
+    background(150);
+}
+function modelLoaded() {
+    console.log("posenet is initialized");
+}
+function getPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+    }
 }
