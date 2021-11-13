9962aded2322b57451a950f70fe0f9ff2bcc80e0
aisha100708 <71584539+aisha100708@users.noreply.github.com>
2021-11-13 16:47:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+var noseX = 0;
+var noseY = 0;
+var leftWrist_X = 0;
+var rightWristX = 0;
+var difference = 0;
 function setup() {
     canvas = createCanvas(600, 500);
     canvas.center();
@@ -8,14 +13,21 @@ function setup() {
     posenet = ml5.poseNet(video, modelLoaded);
     posenet.on('pose', getPoses);
 }
-function draw() {
-    background(150);
-}
 function modelLoaded() {
     console.log("posenet is initialized");
 }
 function getPoses(results) {
     if(results.length > 0) {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        leftWrist_X = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWrist_X - rightWristX);
     }
+}
+function draw() {
+    background(150);
+    fill("pink");
+    square(noseX, noseY, difference);
 }
